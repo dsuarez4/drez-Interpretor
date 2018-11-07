6dc7be86caa75235ad39925fa34a1f1a27e1ef6d
David Suarez <david.suarez0913@gmail.com>
2018-11-06 21:20:20 -0500

    repl and main.go added

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"drez_interpretor/repl"
+	"fmt"
+	"os"
+	"os/user"
+)
+
+func main() {
+
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Drez programming language!\n",
+		user.Username)
+
+	repl.Start(os.Stdin, os.Stdout)
+}
