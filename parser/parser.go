66625c178e6b76f9e8324d7dfb48ec562c2f83e1
David Suarez <david.suarez0913@gmail.com>
2018-11-12 18:20:20 -0500

    tmp

---
+++ parser/parser.go
@@ -0,0 +1,33 @@
+package parser
+
+import (
+	"drez_interpretor/ast"
+	"drez_interpretor/lexer"
+	"drez_interpretor/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l:l}
+
+	// Read Twice, so current token and peek token are set.
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
